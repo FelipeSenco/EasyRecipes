7f73aaf819a98b2ec24eb26d7b15c07b67e9235c
Felipe de Senço <felipedesenco@gmail.com>
2023-03-09 21:27:57 +0000

    first commit

---
+++ App_View/src/App.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import React from "react";
+
+const App: FC = () => {
+  return <h1 data-testid="app-component">Hello World!</h1>;
+};
+
+export default App;
---
+++ App_View/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/", // added publicPath option
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    open: true,
+  },
+};
